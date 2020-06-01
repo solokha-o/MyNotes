2f3f7714bf2ced94a4f5c6fe9fcce8490144b2e5
Oleksandr Solokha <oleksandr.solokha@gmail.com>
2020-06-01 20:39:50 +0300

    Create struct NoteModel with property
    create struct CRUDModel to work with core data
    
    create DetailNoteTableViewController
    
    create MyNotesTableViewController
    
    in storyboard create base model

---
+++ MyNotes/ NoteModel.swift
@@ -0,0 +1,17 @@
+//
+//   NoteModel.swift
+//  MyNotes
+//
+//  Created by Oleksandr Solokha on 01.06.2020.
+//  Copyright © 2020 Oleksandr Solokha. All rights reserved.
+//
+
+import Foundation
+
+struct  NoteModel {
+    let title : String
+    let body : String
+    let date : String
+    let favorite : Bool
+    let id : String
+}
---
+++ MyNotes/CRUDModel.swift
@@ -0,0 +1,14 @@
+//
+//  CRUDModel.swift
+//  MyNotes
+//
+//  Created by Oleksandr Solokha on 01.06.2020.
+//  Copyright © 2020 Oleksandr Solokha. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+struct CRUDModel {
+    
+}
