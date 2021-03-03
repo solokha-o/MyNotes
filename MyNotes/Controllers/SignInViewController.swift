34849b1a1c68efd9ad6b751ed06412620cb033f7
Oleksandr Solokha <oleksandr.solokha@gmail.com>
2021-03-03 07:59:46 +0200

    in SignInViewController configure state of controller: sign-in and sing-up add all components of controller add sign-in button outlet configure sing-up button action configure controller and all his components

--- MyNotes/Controllers/SignInViewController.swift
+++ MyNotes/Controllers/SignInViewController.swift
@@ -12,13 +12,106 @@ import GoogleSignIn
 
 class SignInViewController: UIViewController {
     
+    //configure state of controller: sign-in and sing-up
+    enum State {
+        case signIn, signUp
+        
+        var authorisationLabelTitle: String {
+            switch self {
+                case .signIn: return "Sign-in MyNotes"
+                case .signUp: return "Sign-up MyNotes"
+            }
+        }
+        
+        var signInButtonOutletTitle: String {
+            switch self {
+                case .signIn: return "Sign-in"
+                case .signUp: return "Sign-up"
+            }
+        }
+        
+        var signUpLabelState: Bool {
+            switch self {
+                case .signIn: return false
+                case .signUp: return true
+            }
+        }
+        
+        var signUpButtonOutletTitle: String {
+            switch self {
+                case .signIn: return "Sing-up"
+                case .signUp: return "Cancel"
+            }
+        }
+        
+        var signInByGoogleButtonOutletState: Bool{
+            switch self {
+                case .signIn: return false
+                case .signUp: return true
+            }
+        }
+    }
+    
+    var currentState: State = .signIn
+    
+    
+    //add all components of controller
+    @IBOutlet weak var authorisationLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButtonOutlet: UIButton!
+    @IBOutlet weak var signUpLabel: UILabel!
+    @IBOutlet weak var signUpButtonOutlet: UIButton!
     //add sign-in button outlet
-    @IBOutlet weak var signInButtonOutlet: GIDSignInButton!
+    @IBOutlet weak var signInByGoogleButtonOutlet: GIDSignInButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //make controller like presenting controller google sign-in
         GIDSignIn.sharedInstance()?.presentingViewController = self
+        
+        setupController()
+    }
+    
+    @IBAction func signInButtonAction(_ sender: UIButton) {
+    }
+    //configure sing-up button action
+    @IBAction func signUpButtonAction(_ sender: UIButton) {
+        switch currentState {
+            case .signIn:
+                currentState = .signUp
+                UIView.animate(withDuration: 0.5, animations: {
+                    self.setupController()
+                })
+            case .signUp:
+                currentState = .signIn
+                UIView.animate(withDuration: 0.5, animations: {
+                    self.setupController()
+                })
+        }
+    }
+    //configure controller and all his components
+    func setupController() {
+        switch currentState {
+            case .signIn:
+                authorisationLabel.text = currentState.authorisationLabelTitle
+                signInButtonOutlet.setTitle(currentState.signInButtonOutletTitle, for: .normal)
+                signUpLabel.isHidden = currentState.signUpLabelState
+                signUpButtonOutlet.setTitle(currentState.signUpButtonOutletTitle, for: .normal)
+                signInByGoogleButtonOutlet.isHidden = currentState.signInByGoogleButtonOutletState
+            case .signUp:
+                authorisationLabel.text = currentState.authorisationLabelTitle
+                signInButtonOutlet.setTitle(currentState.signInButtonOutletTitle, for: .normal)
+                signUpLabel.isHidden = currentState.signUpLabelState
+                signUpButtonOutlet.setTitle(currentState.signUpButtonOutletTitle, for: .normal)
+                signInByGoogleButtonOutlet.isHidden = currentState.signInByGoogleButtonOutletState
+        }
+        emailLabel.text = "E-mail"
+        passwordLabel.text = "Password"
+        signUpLabel.text = "If you not sign-up press ⬇️"
+        signUpButtonOutlet.setTitle("Sign-up", for: .normal)
     }
     
     /*
